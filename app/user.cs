4b94110b550aa38d605568cd3d1398f92933216a
Gabriel <77118197+Noozajureg@users.noreply.github.com>
2023-09-06 16:59:29 -0300

    update do app

---
+++ app/user.cs
@@ -0,0 +1,16 @@
+using System.Security.AccessControl;
+
+namespace UserType{
+    public class User {
+        public string Email { get; set;}
+        public string Password { get; set;}
+        public int ID { get; set; }
+
+        public User()
+        {
+            Email = "vdaasdda";
+            Password = "adads";
+            ID = 1;
+        }
+    }
+}
