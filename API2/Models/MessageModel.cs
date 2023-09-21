534425c2f2e7dd3e7027f6d71a821e976b02eba7
Gabriel <77118197+Noozajureg@users.noreply.github.com>
2023-09-21 16:21:27 -0300

    Integracao com a api para o envio de mensagens
    Falta so fazer os relatorios e polir o codigo, na segunda devo conseguir entregar :P

---
+++ API2/Models/MessageModel.cs
@@ -0,0 +1,27 @@
+using UserClass;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MessageModel
+{
+    public class MessageModel{
+        // ver com o diego se as respostas devem ser respondidas na plataforma ou se devem pegar de numeros reais
+        public static async Task<string> SendMessageAsync(int ID, string phone_number, string message){
+            string url = "https://sms.comtele.com.br/api/v2/send";
+            using (HttpClient httpClient = new()){
+                httpClient.DefaultRequestHeaders.Add("auth-key", "7888ffef-72f1-45ce-bd7f-82faf12d7fd6");
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string payload = $"{{\"Sender\":\"{ID}\",\"Receivers\":\"{phone_number}\",\"Content\":\"{message}\"}}";
+                HttpContent content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+                using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url)){
+                        request.Content = content;
+                        HttpResponseMessage response = await httpClient.SendAsync(request);
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        return responseContent;
+                }
+            }
+        }
+    }
+}
---
+++ API2/Models/UserClass.cs
@@ -0,0 +1,11 @@
+namespace UserClass{
+    public class User {
+        public string ACCOUNTTYPE{get; set;}
+        public int? ID { get; set; }
+         public User()
+        {
+            ID = null;
+        }
+    }
+
+}
